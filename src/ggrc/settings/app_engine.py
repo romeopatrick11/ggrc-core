1ae2c0d16479ca4f19d68918b8d7abf725aa4f27
Alieh Rymasheuski <alieh.rymasheuski@gmail.com>
2017-04-28 16:14:44 +0300

    Add client id authentication to cloud endpoint
    When settings.DEBUG is True, API Explorer has access too.

--- src/ggrc/settings/app_engine.py
+++ src/ggrc/settings/app_engine.py
@@ -1,6 +1,8 @@
 # Copyright (C) 2017 Google Inc.
 # Licensed under http://www.apache.org/licenses/LICENSE-2.0 <see LICENSE file>
 
+import os
+
 APP_ENGINE = True
 ENABLE_JASMINE = False
 LOGIN_MANAGER = 'ggrc.login.appengine'
@@ -11,3 +13,5 @@ SQLALCHEMY_RECORD_QUERIES = False
 MEMCACHE_MECHANISM = True
 CALENDAR_MECHANISM = False
 BACKGROUND_COLLECTION_POST_SLEEP = 2.5  # seconds
+ALLOWED_CLOUD_ENDPOINTS_CLIENT_IDS = (
+    os.environ.get("ALLOWED_CLOUD_ENDPOINTS_CLIENT_IDS", "").split())
